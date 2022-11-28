2d6e78c1ed8da46a484cd791a0fe0c64570eb268
AbdullohMahkamov <abdullamn1996@gmail.com>
2022-11-28 10:18:57 +0500

    forPorfolio

---
+++ precache-manifest.315797d0962ace18c48c35930377982d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93923321372966b421f024c6ef4376a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1681ba2c0c1b87ee22b9",
+    "url": "/static/css/main.b79e02fc.chunk.css"
+  },
+  {
+    "revision": "d3ddbb620261aa50c1a5",
+    "url": "/static/js/2.601808eb.chunk.js"
+  },
+  {
+    "revision": "49b611756b7cdcd9ec9914f14c49889c",
+    "url": "/static/js/2.601808eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1681ba2c0c1b87ee22b9",
+    "url": "/static/js/main.4233b4fb.chunk.js"
+  },
+  {
+    "revision": "35d8ea8e0de0fd66811a",
+    "url": "/static/js/runtime-main.bb80f66d.js"
+  },
+  {
+    "revision": "5548f3e55ab6d95c4b4f29ec6e3b521a",
+    "url": "/static/media/Highlight.5548f3e5.png"
+  },
+  {
+    "revision": "7c8a92f49c7aac73ad7f884c7830d9d0",
+    "url": "/static/media/Rectangle.7c8a92f4.png"
+  }
+]);
